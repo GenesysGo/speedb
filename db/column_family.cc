44028d511dcb587df2012164c1cadef122004796
Yuval Ariel <yuval@speedb.io>
2024-01-31 11:51:29 +0200

    Auto Tuning: Set delayed write rate according to L0 clearance.
    Then decrease write rate with factor adjust.
    
    Calculate the delayed write rate based on the total time L0 was in need to compaction
    (L0 files above trigger) and the amount of L0 data moved to L1.

--- db/column_family.cc
+++ db/column_family.cc
@@ -25,6 +25,7 @@
 
 #include <algorithm>
 #include <cinttypes>
+#include <cmath>
 #include <functional>
 #include <limits>
 #include <memory>
@@ -901,8 +902,9 @@ int GetL0ThresholdSpeedupCompaction(int level0_file_num_compaction_trigger,
 }  // anonymous namespace
 
 namespace {
-const int kMemtablePenalty = 10;
+const int kMemtablePenalty = 5;
 const int kNumPendingSteps = 100;
+const double kGoalMbs = 5242880.0;
 }  // namespace
 
 double ColumnFamilyData::TEST_CalculateWriteDelayDivider(
@@ -917,6 +919,7 @@ void ColumnFamilyData::DynamicSetupDelay(
     uint64_t max_write_rate, uint64_t compaction_needed_bytes,
     const MutableCFOptions& mutable_cf_options,
     WriteStallCause& write_stall_cause) {
+  assert(max_write_rate > 0);
   const double rate_divider =
       CalculateWriteDelayDividerAndMaybeUpdateWriteStallCause(
           compaction_needed_bytes, mutable_cf_options, write_stall_cause);
@@ -1009,6 +1012,7 @@ ColumnFamilyData::CalculateWriteDelayDividerAndMaybeUpdateWriteStallCause(
         1 / (1 - (static_cast<double>(step_num) / kNumPendingSteps));
   }
 
+  pending_divider = 1;
   double biggest_divider = 1;
   if (memtable_divider > pending_divider) {
     biggest_divider = memtable_divider;
@@ -1024,16 +1028,55 @@ ColumnFamilyData::CalculateWriteDelayDividerAndMaybeUpdateWriteStallCause(
   }
 
   // L0 files
+  //
+  // L0 files increase steadily and then drop in a batch when the L0L1 / intra
+  // compaction ends. We'd like to avoid steadily decreasing the write speed
+  // along with the L0 accumulation and then increasing it heavily when the
+  // compaction ends.
+  // Another consideration is to never reach level0_stop_writes_trigger.
+  // The initial L0 delay write rate is set according to the speed L0 can
+  // clear data which is the amount of data moved from L0 to L1 divided by the
+  // time it was above compaction trigger.
+  // This is calculated and set when the L0L1 compaction ends in
+  // CompactionJob::Install.
+  // Use this rate for the first extra slowdown files and then heavily delay the
+  // rest of the files. In any case, delay at least 2
+  // * trigger.
+
+  // slowdown 12 ; stop 30; - start at 22
+  // slowdown 12 ; stop 50; - start at 24
+  // slowdown 12 ; stop 16; start at 12
+  auto slowdown = mutable_cf_options.level0_slowdown_writes_trigger;
+  auto stop = mutable_cf_options.level0_stop_writes_trigger;
+  auto trigger = mutable_cf_options.level0_file_num_compaction_trigger;
+
+  auto file_to_start_delay = slowdown;
+  auto gap = stop - slowdown;
+  if ((gap > (slowdown + 2 * trigger))) {
+    file_to_start_delay = slowdown * 2;
+  } else if (stop - (2 * trigger) > slowdown) {
+    file_to_start_delay = stop - (2 * trigger);
+  } else {
+    file_to_start_delay = slowdown;
+  }
+  // The goal is to reach kGoalMbs 3 steps before stop condition so that we
+  // never reach it. The formula to decide the delay percentage is: kGoalMbs =
+  // start_rate * (delay_percent ^ num_steps) where: kGoalMbs = 5 , start_rate
+  // is the l0_compaction_speed num_steps = num_L0_steps - 3.
   double l0_divider = 1;
-  const auto extra_l0_ssts = vstorage->l0_delay_trigger_count() -
-                             mutable_cf_options.level0_slowdown_writes_trigger;
+  const auto extra_l0_ssts =
+      vstorage->l0_delay_trigger_count() - file_to_start_delay;
   if (extra_l0_ssts > 0) {
-    const auto num_L0_steps = mutable_cf_options.level0_stop_writes_trigger -
-                              mutable_cf_options.level0_slowdown_writes_trigger;
+    const auto num_L0_steps = stop - file_to_start_delay;
     assert(num_L0_steps > 0);
+    double num_steps = num_L0_steps > 6 ? num_L0_steps - 3 : num_L0_steps;
+    double delay_percent =
+        std::pow((kGoalMbs / l0_base_compaction_speed()), 1.0 / num_steps);
     // since extra_l0_ssts == num_L0_steps then we're in a stop condition.
+    assert(delay_percent > 0 && delay_percent < 1);
     assert(extra_l0_ssts < num_L0_steps);
-    l0_divider = 1 / (1 - (static_cast<double>(extra_l0_ssts) / num_L0_steps));
+
+    l0_divider = 1 / (std::pow(delay_percent, extra_l0_ssts));
   }
 
   if (l0_divider > biggest_divider) {
@@ -1042,6 +1085,20 @@ ColumnFamilyData::CalculateWriteDelayDividerAndMaybeUpdateWriteStallCause(
   }
 
   return biggest_divider;
+}
+
+void ColumnFamilyData::SetL0BaseCompactionSpeed(uint64_t size) {
+  assert(l0_start_clearance_time_ > 0);
+  uint64_t l0_clearance_dur =
+      ioptions_.clock->NowMicros() - l0_start_clearance_time_;
+  assert(l0_clearance_dur > 0);
+  lo_base_compaction_speed_ = size / l0_clearance_dur;
+  started_l0_timer_ = false;
+  l0_start_clearance_time_ = 0;
+  ROCKS_LOG_WARN(ioptions_.logger,
+                 "L0L1 compaction ended. duration : %" PRIu64
+                 " . amount cleared: %" PRIu64,
+                 l0_clearance_dur, size);
 }
 
 WriteStallCondition ColumnFamilyData::RecalculateWriteStallConditions(
@@ -1069,10 +1126,31 @@ WriteStallCondition ColumnFamilyData::RecalculateWriteStallConditions(
     // calculate all conditions with DynamicSetupDelay and reevaluate the
     // write_stall_cause. this is only relevant in the kDelayed case.
     if (dynamic_delay) {
+      // AutoTuneDelayedWriteRate();
+
+      // reset time point when L0L1 finishes and calc speed.
+      // when compaction ends, and set_l0_size_cleared is called, we calc the
+      // rate.
+
+      // start timer for L0 clearance when trigger passed.
+      if (!started_l0_timer_ &&
+          vstorage->l0_delay_trigger_count() >
+              mutable_cf_options.level0_file_num_compaction_trigger) {
+        started_l0_timer_ = true;
+        l0_start_clearance_time_ = ioptions_.clock->NowMicros();
+      }
+
+      // reset timer when theres less L0 files than trigger.
+      if (started_l0_timer_ &&
+          vstorage->l0_delay_trigger_count() <
+              mutable_cf_options.level0_file_num_compaction_trigger) {
+        started_l0_timer_ = false;
+        l0_start_clearance_time_ = 0;
+      }
+
       if (write_stall_condition == WriteStallCondition::kDelayed) {
-        DynamicSetupDelay(write_controller->max_delayed_write_rate(),
-                          compaction_needed_bytes, mutable_cf_options,
-                          write_stall_cause);
+        DynamicSetupDelay(l0_base_compaction_speed(), compaction_needed_bytes,
+                          mutable_cf_options, write_stall_cause);
         write_controller_token_.reset();
       } else {
         write_controller->HandleRemoveDelayReq(this);
